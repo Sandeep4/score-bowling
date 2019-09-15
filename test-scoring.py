cf30a035b5146416b0915b98c91944bac622cf31
Sandeep Kumar <4sandeep.kumar@gmail.com>
2019-09-15 14:18:00 +0800

    Add test case for wrong result

--- test-scoring.py
+++ test-scoring.py
@@ -1,15 +1,23 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+
+from score import CompletedScoreCard, ScoreCard, ExcessFramesException, IllegalResultException
 import unittest
-from score import CompletedScoreCard, ScoreCard, ExcessFramesException
+
 
 
 class NegativeBowlingTests(unittest.TestCase):
 
     def test_excess_frames(self):
-        results = ["X","X","X","X","X","X","X","X","X","X","XXX"]
+        results = ['X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'XXX']
         self.assertRaises(ExcessFramesException, CompletedScoreCard,
                           ScoreCard([]), results)
+
+    def test_wrong_result(self):
+        results = ['XX', '23', '6/', '7/']
+        self.assertRaises(IllegalResultException, CompletedScoreCard,
+                          ScoreCard([]), results)
+
 
 
 if __name__ == '__main__':
