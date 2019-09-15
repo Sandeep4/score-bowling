38c8faaec799c6bbfdc70a994343ff6f36cae16b
Sandeep Kumar <4sandeep.kumar@gmail.com>
2019-09-15 14:31:35 +0800

    Add test cases for scoring frames

--- test-scoring.py
+++ test-scoring.py
@@ -5,6 +5,38 @@ from score import CompletedScoreCard, ScoreCard, ExcessFramesException, IllegalR
 import unittest
 
 
+class PositiveBowlingTests(unittest.TestCase):
+
+    def test_strikes(self):
+        results = ['X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'XXX']
+        finalScoreCard = CompletedScoreCard(ScoreCard([]), results)
+        self.assertEqual(finalScoreCard.frames[-1].total, 300)
+
+    def test_nearly_all_strikes(self):
+        results = ['X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X00']
+        finalScoreCard = CompletedScoreCard(ScoreCard([]), results)
+        self.assertEqual(finalScoreCard.frames[-1].total, 270)
+
+    def test_scores(self):
+        results = ['90', '90', '90', '90', '90', '90', '90', '90', '90', '90']
+        finalScoreCard = CompletedScoreCard(ScoreCard([]), results)
+        self.assertEqual(finalScoreCard.frames[-1].total, 90)
+
+    def test_spares(self):
+        results = ['5/', '5/', '5/', '5/', '5/', '5/', '5/', '5/', '5/', '5/5']
+        finalScoreCard = CompletedScoreCard(ScoreCard([]), results)
+        self.assertEqual(finalScoreCard.frames[-1].total, 150)
+
+    def test_no_extra_point(self):
+        results = ['5/']
+        finalScoreCard = CompletedScoreCard(ScoreCard([]), results)
+        self.assertEqual(finalScoreCard.frames[-1].total, 10)
+
+    def test_game_example(self):
+        results = ['X', '7/', '72', '9/', 'X', 'X', 'X', '23', '6/', '7/3']
+        finalScoreCard = CompletedScoreCard(ScoreCard([]), results)
+        self.assertEqual(finalScoreCard.frames[-1].total, 168)
+
 
 class NegativeBowlingTests(unittest.TestCase):
 
@@ -18,6 +50,10 @@ class NegativeBowlingTests(unittest.TestCase):
         self.assertRaises(IllegalResultException, CompletedScoreCard,
                           ScoreCard([]), results)
 
+    def test_wrong_result(self):
+    	results = ['X', '7/', '72', '9/', 'X', 'X', 'X', '23', '6/', 'XX']
+        self.assertRaises(IllegalResultException, CompletedScoreCard,
+                          ScoreCard([]), results)
 
 
 if __name__ == '__main__':
