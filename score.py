f9bdcc1569f6748128ffd01b20af1847740a0aea
Sandeep Kumar <4sandeep.kumar@gmail.com>
2019-09-15 14:23:24 +0800

    Add frameScoring stub and input validation method

--- score.py
+++ score.py
@@ -30,9 +30,33 @@ class ScoreCard:
 class ExcessFramesException(Exception):
     pass
 
+class IllegalResultException(Exception):
+	pass    
+
+def validateResult(result, scorecard_size):
+    if len(result) == 1 and result != 'X' or len(result) > 3:
+        raise IllegalResultException('Wrong result for frame no:',
+                scorecard_size + 1, result)
+    if scorecard_size != 9 and len(result) == 3:
+        raise IllegalResultException('Wrong result for frame no:',
+                scorecard_size + 1, result)
+    if result[0] == 'X' and len(result) > 1:
+        if scorecard_size < 9:
+            raise IllegalResultException('Wrong result for frame no:',
+                    scorecard_size + 1, result)
+        elif len(result) != 3:
+            raise IllegalResultException('Wrong result for frame no:',
+                    scorecard_size + 1, result)
+
+def scoreFrame(scorecard, result):
+	scorecard_size = scorecard.size()
+	validateResult(result, scorecard_size)
+	
 # Returns a scorecard given a list of results 
 def CompletedScoreCard(scorecard, results):
     if not results:
         return scorecard
     if len(results) > 10:
         raise ExcessFramesException('Frames in game exceded')
+    return CompletedScoreCard(scoreFrame(scorecard, results[0]),
+                              results[1:])
